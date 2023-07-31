d4e63944abc63cb8ef02de0db4c728df84dccfdf
Mavalosolv <139020030+Mavalosolv@users.noreply.github.com>
2023-07-31 09:59:05 -0600

    Practicas de JavaScript modificaciones

---
+++ Mayusculas.js
@@ -0,0 +1,9 @@
+console.log('******* Convertir texto de minusculas a Mayusculas ********');
+
+let tex='navez campeones';
+
+console.log('Texto a convertir: '+tex);
+console.log('Resultado: ' + tex.toUpperCase());
+
+
+console.log('------------------------------------------');
---
+++ Reves.js
@@ -0,0 +1,12 @@
+
+console.log('******* Texto convertido al Reves ********');
+
+let palabra= 'Zologico';
+//let palabra= prompt();
+let respal;
+
+console.log('Text a convertir: '+palabra);
+respal=Array.from(palabra).reverse().join('');
+console.log('Resultado: '+ respal);
+
+console.log('------------------------------------------');

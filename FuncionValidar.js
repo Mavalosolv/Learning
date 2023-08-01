7784fd2ceca085c89685b8136aea0e70cdcee720
Carlos Marin <cmaring1994@gmail.com>
2023-07-31 22:17:36 -0600

    pulir codigo

--- FuncionValidar.js
+++ FuncionValidar.js
@@ -1,15 +1,13 @@
+function validarima(input){
+  let imagen = input.files[0]
+  let arr = ['image/jpg','image/jpeg','image/png']
 
-    function validarima(input){
-
-    let imagen = input.files[0]
-    let arr = ['image/jpg','image/jpeg','image/png']
-
-    if(arr.indexOf(imagen.type) != -1){
+  if(arr.indexOf(imagen.type) != -1){
     console.log('Es Valido')
-}else{
+  }else{
     input.value =""
     console.log('No es Valido')
     alert('Selecciona un archivo valido')
-}
+  }
     console.log(input)
 }
